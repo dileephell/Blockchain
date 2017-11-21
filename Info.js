3ce755de6dde985df25f0aa052f40a9345e50a66
Dileep Singh <dileep2008apex@gmail.com>
2017-11-21 12:29:04 +0530

    Update Info.js

--- Info.js
+++ Info.js
@@ -33,3 +33,24 @@ https://www.udemy.com/ethereum-developer/
 
 
 https://github.com/ethereum/wiki/wiki/JavaScript-API
+
+
+http://forum.ethereum.org/discussion/1317/reentrant-contracts
+http://hackingdistributed.com/2016/06/16/scanning-live-ethereum-contracts-for-bugs/
+http://hackingdistributed.com/2016/06/18/analysis-of-the-dao-exploit/
+http://hackingdistributed.com/2016/06/22/smart-contract-escape-hatches/
+http://martin.swende.se/blog/Devcon1-and-contract-security.html
+http://publications.lib.chalmers.se/records/fulltext/234939/234939.pdf
+http://vessenes.com/deconstructing-thedao-attack-a-brief-code-tour
+http://vessenes.com/ethereum-griefing-wallets-send-w-throw-considered-harmful
+http://vessenes.com/more-ethereum-attacks-race-to-empty-is-the-real-deal
+https://blog.blockstack.org/simple-contracts-are-better-contracts-what-we-can-learn-from-the-dao-6293214bad3a
+https://blog.slock.it/deja-vu-dao-smart-contracts-audit-results-d26bc088e32e
+https://github.com/Bunjin/Rouleth/blob/master/Security.md
+https://github.com/LeastAuthority/ethereum-analyses
+https://medium.com/@ConsenSys/assert-guards-towards-automated-code-bounties-safe-smart-contract-coding-on-ethereum-8e74364b795c
+https://medium.com/@coriacetic/in-bits-we-trust-4e464b418f0b
+https://medium.com/@hrishiolickel/why-smart-contracts-fail-undiscovered-bugs-and-what-we-can-do-about-them-119aa2843007
+https://medium.com/@peterborah/we-need-fault-tolerant-smart-contracts-ec1b56596dbc
+https://pdaian.com/blog/chasing-the-dao-attackers-wake
+http://www.comp.nus.edu.sg/~loiluu/papers/oyente.pdf
